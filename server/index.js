7c3da2002660df3d1e361d18d6fb87d529eb0d6b
Franco Caraffa <francocaraffa2005@gmail.com>
2024-04-08 22:59:28 -0300

    Añadiendo funcion de saveMessage y getMessages

--- server/index.js
+++ server/index.js
@@ -1,6 +1,31 @@
 import express from "express"
 import {Server as SocketServer} from "socket.io"
 import http from "http"
+import mongoose from "mongoose"
+
+mongoose.connect("mongodb://localhost:27017")
+
+const messageSchema = new mongoose.Schema({
+    message: {
+        type: String,
+        required: true
+    }
+})
+
+const Message = mongoose.model("Message", messageSchema)
+
+async function getMessages(){
+    const messages = await Message.find()
+    
+    return messages
+}
+
+async function saveMessage(data){
+    const message = new Message({
+        message: data
+    })
+    message.save().then(res => console.log("Usuario creado exitosamente")).catch(error => console.log(error))
+}
 
 const app = express()
 const server = http.createServer(app)
@@ -19,11 +44,15 @@ io.on("connect", socket => {
             data,
             from: socket.id.slice(13)
         })
+        saveMessage(data)
     })
 })
 
-
-
+app.get("/getMessages", async (req, res) => {
+    const messages = await getMessages()
+    console.log(messages)
+    res.send(messages)
+})
 
 
 server.listen(3000, ()=> console.log("Working"))
