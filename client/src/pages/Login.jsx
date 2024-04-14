7d1e0d1e319ddde30441052153a741e974f7007f
Franco Caraffa <francocaraffa2005@gmail.com>
2024-04-14 20:01:41 -0300

    bug fixed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,7 +8,7 @@ export function Login({setRender}){
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const response = await fetch('http://localhost:3000/api/login', {
+        const response = await fetch('https://websockets-chat-backend.onrender.com/api/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
